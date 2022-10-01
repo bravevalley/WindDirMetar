a4d84a3f3c8821468ad57f9ebd9d74cb1649630f
Dassyareg <dassyareg@gmail.com>
2022-10-01 19:39:29 +0100

    Extracted the Wind Intergers

--- main.go
+++ main.go
@@ -5,23 +5,106 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
-	dataFile = regexp.MustCompile(`sample`)
+	dataFile  = regexp.MustCompile(`sample`)
+	validLine = regexp.MustCompile(`^JFK*`)
+	windMetar = regexp.MustCompile(`\d*KT`)
+	windDir = regexp.MustCompile(`\d{3}`)
 )
 
 // Get the text
 
-// Seperate the text and pass the stream lined text
+// 2. Seperate the text and pass the stream lined text
 
-// Extract the information from stream line
+func textProcessing(textProcessSender <-chan string, sendProcessedText chan<- string) {
+	for rawStringData := range textProcessSender {
+
+		lines := strings.Split(rawStringData, "\n")
+		for _, singleLine := range lines {
+	
+			// JFK,2012-04-24 07:51,42.98,33.80,69.80,230.00,15.00,0.00,29.45,997.
+			// 20,10.00,M,BKN,M,M,M,2700.00,M,M,M,M,M,M,M,M,M,M,35.01,KJFK 240751Z
+			// 23015KT 10SM BKN027 06/01 A2945 RMK SLP972 T0061 0011,M
+	
+			// Ensure it is not a comment or heading
+			if !validLine.MatchString(singleLine) {
+				continue
+			}
+	
+			// Send the data a Line at a time
+			sendProcessedText <- singleLine
+		}
+	}
+
+	// Close the channel feeding the processed string
+	close(sendProcessedText)
+	
+}
+
+// 3. Extract the information from stream line
+
+func extractWindInfo(processTextReceiver <-chan string, extractedWindChan chan<- string) {
+	// Get incoming data from the previous process
+
+	// 70,10.00,M,FEW,OVC,M,M,4000.00,9500.00,M,M,-PL,M,M,M,M,M,M,31.71,KJFK 282251Z 07013KT
+
+	for text := range processTextReceiver {
+
+		// Match and extract the Wind Data needed for our calculation
+		// [07013KT]
+		windMetaData := windMetar.FindAllString(text, 1)
+
+		// Extract the string from the wind metar []string
+		for _, windData := range windMetaData {
+
+			// Send the extracted wind information to the next thread - 07013KT
+			extractedWindChan <- windData
+		}
+	}
+
+	// Close the channel feeding the extracted wind data
+	close(extractedWindChan)
+}
+
+func extractWindDir(extractedWindReceiver <-chan string, windDirChan chan<- int) {
+	for windData := range extractedWindReceiver {
+		winDirExtract := windDir.FindAllString(windData, 1)
+		for _, windDirData := range winDirExtract {
+			windDirDataInt, err := strconv.Atoi(windDirData)
+			if err != nil {
+				fmt.Println("Error parsing String to Interger")
+			}
+			fmt.Printf("%v\n\n", windDirDataInt)
+		
+				
+			windDirChan <- windDirDataInt
+		}
+			
+		
+	}
+}
 
 // Collate information from the extracted information
 
 func main() {
+	textProcessSender := make(chan string)
+	textProcessedSender := make(chan string)
+	extractedWindSender := make(chan string)
+	extractedWindDirSender := make(chan int)
 
-	// Get the text
+	start := time.Now()
+
+	go textProcessing(textProcessSender, textProcessedSender)
+
+	go extractWindInfo(textProcessedSender, extractedWindSender)
+
+	go extractWindDir(extractedWindSender, extractedWindDirSender)
+	// 1. Get the text
 
 	// Define the absolute path to the Directory where the data
 	// is located
@@ -44,9 +127,6 @@ func main() {
 		// Check which of the file matches the name of our data file
 		if dataFile.MatchString(files.Name()) {
 
-			// Informal unit test
-			fmt.Println("We found him")
-
 			// Define the path to the data file
 			// NOTE: I could have defined the path and read the file
 			// without reading the Dir first, I chose this route because
@@ -63,8 +143,21 @@ func main() {
 				fmt.Println("Error reading file")
 			}
 
-			fmt.Println(string(file))
+			text := string(file)
+
+			textProcessSender <- text
 		}
+
 	}
+
+	close(textProcessSender)
+
+	for v := range extractedWindDirSender {
+		fmt.Printf("%v", v)
+		
+	} 
+	end := time.Since(start)
+
+	fmt.Println(end)
 
 }
